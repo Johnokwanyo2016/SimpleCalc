24315bd1eb85bfb91b24739eb897bf253a2057ce
johnokwanyo2016 <johnokwanyo2016@gmail.com>
2024-05-28 11:09:07 +0300

    changes

--- app/src/main/java/com/example/simplecalc/MainActivity.kt
+++ app/src/main/java/com/example/simplecalc/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.simplecalc
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
